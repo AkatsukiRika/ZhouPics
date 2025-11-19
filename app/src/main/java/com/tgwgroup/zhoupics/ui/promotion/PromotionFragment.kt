f3b9a298e97b2285c04cb9d11408768375f03d10
AkatsukiRika <m1345860061@gmail.com>
2025-11-19 15:42:29 +0800

    Feat: Open link in browser

--- app/src/main/java/com/tgwgroup/zhoupics/ui/promotion/PromotionFragment.kt
+++ app/src/main/java/com/tgwgroup/zhoupics/ui/promotion/PromotionFragment.kt
@@ -1,7 +1,9 @@
 package com.tgwgroup.zhoupics.ui.promotion
 
+import com.tgwgroup.zhoupics.R
 import com.tgwgroup.zhoupics.base.BaseFragment
 import com.tgwgroup.zhoupics.databinding.FragmentPromotionBinding
+import com.tgwgroup.zhoupics.utils.openUrl
 
 class PromotionFragment : BaseFragment<FragmentPromotionBinding>() {
     companion object {
@@ -10,5 +12,19 @@ class PromotionFragment : BaseFragment<FragmentPromotionBinding>() {
 
     override fun onBindingCreate(): FragmentPromotionBinding {
         return FragmentPromotionBinding.inflate(layoutInflater)
+    }
+
+    override fun initView() {
+        val context = context ?: return
+        val binding = binding ?: return
+        binding.clCard1.setOnClickListener {
+            context.openUrl(context.resources.getString(R.string.github_link))
+        }
+        binding.clCard2.setOnClickListener {
+            context.openUrl(context.resources.getString(R.string.personal_website_link))
+        }
+        binding.clCard3.setOnClickListener {
+            context.openUrl(context.resources.getString(R.string.zhoutools_link))
+        }
     }
 }
--- app/src/main/java/com/tgwgroup/zhoupics/utils/AppUtils.kt
+++ app/src/main/java/com/tgwgroup/zhoupics/utils/AppUtils.kt
@@ -1,13 +1,26 @@
 package com.tgwgroup.zhoupics.utils
 
+import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.os.Parcelable
+import androidx.core.net.toUri
+import com.tgwgroup.zhoupics.R
 
 fun <T : Parcelable> Intent.getParcelableExtraCompat(name: String, clazz: Class<T>): T? {
     return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
         this.getParcelableExtra(name, clazz)
     } else {
         this.getParcelableExtra(name) as? T
+    }
+}
+
+fun Context.openUrl(url: String) {
+    runCatching {
+        val intent = Intent(Intent.ACTION_VIEW, url.toUri())
+        startActivity(intent)
+    }.onFailure {
+        it.printStackTrace()
+        toastError(resources.getString(R.string.unable_to_open_link))
     }
 }
